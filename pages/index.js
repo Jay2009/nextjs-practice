598db53ec217bc8de50e58b5b13d217d4b49a5f0
Jay2009 <jaem2009@naver.com>
2022-01-11 23:48:46 +0900

    ssr, redirect and rewrite and fetching data and dynamic routes done

--- pages/index.js
+++ pages/index.js
@@ -5,27 +5,14 @@ import Seo from "../components/Seo";
 
 const API_KEY = "5729b9af3f709c29897893a275a5789a";
 
-export default function Home() {
-    const [movies, setMovies] = useState([]);
-
-    useEffect(() => {
-        (async () => {
-            const { results } = await (
-            await fetch(
-                `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-            )
-            ).json();
-            setMovies(results);
-        })();
-    }, []);
+export default function Home({results}) {
 
     return (
         <div  className="container">
             <Seo title="Home" />
-            {!movies && <h4>Loading...</h4>}
-            {movies?.map((movie) => (
+            {results?.map((movie) => (
                 <div  className="movie"  key={movie.id}>
-                    <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+                    <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
                     <h4>{movie.original_title}</h4>
                 </div>
             ))}
@@ -42,14 +29,26 @@ export default function Home() {
                     transition: transform 0.2s ease-in-out;
                     box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
                 }
-                    .movie:hover img {
+                .movie:hover img {
                     transform: scale(1.05) translateY(-10px);
                 }
                 .movie h4 {
                     font-size: 18px;
                     text-align: center;
                 }
+                .movie {
+                    cursor: pointer;
+                }
         `}</style>
         </div>
     );
+}
+// getServerSideProps -> name must be this for ssr and this function should return an obj
+export async function getServerSideProps(){
+    const { results } = await (await fetch(`http://localhost:3000/api/movies`)).json();
+    return{
+        props: {
+            results,
+        },
+    };
 }
